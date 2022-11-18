6dcba87c5806af5a91505d380999c778344e9945
AlexParco <alexparco16@gmail.com>
2022-11-18 14:08:46 -0500

    add unique in create table user query

--- pokeapp-api/migrations/000001_init_schema.up.sql
+++ pokeapp-api/migrations/000001_init_schema.up.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS pokefavs CASCADE;
 
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
-  username VARCHAR(32) NOT NULL CHECK (username <> ''),
+  username VARCHAR(32) UNIQUE NOT NULL CHECK (username <> ''),
   password VARCHAR(250)  NOT NULL CHECK (octet_length(password) <> 0), 
   role VARCHAR(10) NOT NULL DEFAULT 'ROLE_USER'
 );
